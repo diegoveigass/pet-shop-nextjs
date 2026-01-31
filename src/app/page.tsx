4330a46b6aa6eb6396486cde49c572d8883b0f25
Diego Veiga <sdiegoveiga@gmail.com>
2026-01-31 11:21:39 -0300

    Creating header component in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-accent-orange">Home</h1>
+    <div className="bg-background-primary p-6">
+      <div className="flex items-center justify-between md:mb-8">
+        <div>
+          <h1 className="text-title text-content-primary mb-2">Sua agenda</h1>
+          <p className="text-content-secondary text-paragraph-small">
+            Aqui você pode ver todos osclientes e serviços agendados para hoje
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
